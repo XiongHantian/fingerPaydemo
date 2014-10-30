4c002c72a91af5201675a41ee90a13b10f97e831
xht <xionght11@gmail.com>
2014-10-30 16:04:44 +0800

    10.30

---
+++ FingerPay/src/com/yiwen/activity/IndexActivity.java
@@ -0,0 +1,19 @@
+package com.yiwen.activity;
+
+import com.yiwen.fingerpay.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IndexActivity extends Activity {
+
+	// ³£Á¿
+	public static final String TAG = "IndexActivity";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_index);
+
+	}
+
+}
